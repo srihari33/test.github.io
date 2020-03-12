dde6378def5fc24038393e87eac6164454553eca
srihari33 <yvsriharikrishna3@gmail.com>
2020-03-12 09:46:01 +0530

    Update test.js

--- test.js
+++ test.js
@@ -112,13 +112,84 @@ var template3 = `
 </body> 
 </html>`
 
+var template4 = `<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box;}
+
+.form-inline {  
+  display: flex;
+  flex-flow: row wrap;
+  align-items: center;
+}
+
+.form-inline label {
+  margin: 5px 10px 5px 0;
+}
+
+.form-inline input {
+  vertical-align: middle;
+  margin: 5px 10px 5px 0;
+  padding: 10px;
+  background-color: #fff;
+  border: 1px solid #ddd;
+}
+
+.form-inline button {
+  padding: 10px 20px;
+  background-color: dodgerblue;
+  border: 1px solid #ddd;
+  color: white;
+  cursor: pointer;
+}
+
+.form-inline button:hover {
+  background-color: royalblue;
+}
+
+@media (max-width: 800px) {
+  .form-inline input {
+    margin: 10px 0;
+  }
+  
+  .form-inline {
+    flex-direction: column;
+    align-items: stretch;
+  }
+}
+</style>
+</head>
+<body>
+
+<center>
+Template 4
+<center>
+
+<form class="form-inline" action="/action_page.php">
+  <label for="email">First Name:</label>
+  <input type="email" id="text" placeholder="first name" name="">
+  <label for="pwd">Last Name:</label>
+  <input type="text" id="pwd" placeholder="last name" name="">
+  <label>
+    <input type="checkbox" name="remember"> Remember me
+  </label>
+  <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
+`
+
 function dynamicLoad(){
 
 Array.prototype.sample = function(){
   return this[Math.floor(Math.random()*this.length)];
 }
-var value = [1, 2, 3].sample();
-templateArray = {1: template1, 2: template2, 3: template3}
+var value = [1, 2, 3, 4].sample();
+templateArray = {1: template1, 2: template2, 3: template3, 4: template4}
 document.body.innerHTML= ""
 
 document.write(templateArray[value]);
